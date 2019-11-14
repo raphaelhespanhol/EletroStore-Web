d702f9fdf6ac3e9eb448e0e16fdfdc647b6f2372
hespanhol <hespanhol@mbp-de-raphael>
2019-11-14 08:47:48 -0200

    Sending the basic parameters for Product entity

--- eletrostore-web/src/components/DetailsProductComponent.vue
+++ eletrostore-web/src/components/DetailsProductComponent.vue
@@ -12,7 +12,11 @@
             </fieldset>
             <fieldset class="form-group">
                 <label>Categoria</label>
-                <input type="text" class="form-control" v-model="id_category">
+                <select class="form-control" v-model="id_category">
+                  <option v-for="category in categories" v-bind:key="category.id" v-bind:value="category.id">
+                    {{category.name}}
+                  </option>
+                </select>
             </fieldset>
             <fieldset class="form-group">
                 <label>Descrição</label>
@@ -23,27 +27,31 @@
                 <input type="text" class="form-control" v-model="price">
             </fieldset>
               <image-uploader
+                :preview="true"
+                :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+                capture="environment"
                 :debug="1"
-                :maxWidth="512"
-                :quality="0.7"
-                :autoRotate=true
+                doNotResize="gif"
+                :autoRotate="true"
                 outputFormat="verbose"
-                :preview=false
-                :className="['fileinput', { 'fileinput--loaded' : hasImage }]"
-                :capture="false"
-                accept="video/*,image/*"
-                doNotResize="['gif', 'svg']"
                 @input="setImage"
-                @onUpload="startImageResize"
-                @onComplete="endImageResize"
-              >
-                <label for="fileInput" slot="upload-label">
+              ><label for="fileInput" slot="upload-label">
                   <figure>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                        <path class="path1" d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"></path>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                    >
+                      <path
+                        class="path1"
+                        d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"
+                      ></path>
                     </svg>
                   </figure>
-                  <span class="upload-caption">{{ hasImage ? 'Replace' : 'Upload' }}</span>
+                  <span class="upload-caption">{{
+                    hasImage ? "Alterar imagem" : "Adicionar imagem"
+                  }}</span>
                 </label>
               </image-uploader>
             <button class="btn btn-success" type="submit">Salvar</button>
@@ -54,13 +62,17 @@
 </template>
 
 <script>
+import CategoryService from '../services/CategoryService';
 import ProductService from '../services/ProductService';
 export default {
   name: "DetailsProductComponent",
   data() {
     return {
       name: "",
-      errors: []
+      hasImage: false,
+      image: null,
+      errors: [],
+      categories: []
     };
   },
   computed: {
@@ -70,11 +82,17 @@ export default {
   },
   methods: {
     refreshProductDetails() {
-        if(this.id > 0) {
-          ProductService.retrieveById(this.id).then(res => {
-            this.name = res.data.name;
-          });
-        }
+      CategoryService.retrieveAll()
+      .then(response => {
+        this.categories = response.data;
+      });
+      if(this.id > 0) {
+        ProductService.retrieveById(this.id).then(res => {
+          this.name = res.data.name;
+          this.id_category = res.data.category.id;
+          this.price = res.data.price;
+        });
+      }
     },
     validateAndSubmit(e) {
       e.preventDefault();
@@ -88,7 +106,10 @@ export default {
       if(this.errors.length === 0) {
         if (this.id == 0) {
           ProductService.create({
-              name: this.name
+              name: this.name,
+              category: {id: this.id_category},
+              price: this.price,
+              image: this.image
           })
           .then(() => {
               this.$router.push('/products');
@@ -96,13 +117,21 @@ export default {
         } else {
           ProductService.update(this.id, {
               id: this.id,
-              name: this.name
+              name: this.name,
+              category: {id: this.id_category},
+              price: this.price,
+              image: this.image
           })
           .then(() => {
               this.$router.push('/products');
           });
         }
       }
+    },
+    setImage: function(output) {
+      this.hasImage = true;
+      this.image = output;
+      console.log(this.image);
     },
     returnProductList() {
       this.$router.push("/products");
